b9c8e6767d8074ed542fa01ff7648ec59e0132cc
MaxiCarrillo <maxi11carrillo@gmail.com>
2023-11-19 23:21:46 -0300

    subida del sitio

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// Obtén el elemento
+let scrollToHome = document.getElementById('scrollToHome');
+
+// Oculta el elemento inicialmente
+scrollToHome.style.display = 'none';
+
+// Agrega el evento de escucha de desplazamiento
+window.addEventListener('scroll', function() {
+    // Verifica la posición de desplazamiento
+    if (window.scrollY > 100) {
+        // Muestra el elemento
+        scrollToHome.style.display = 'block';
+    } else {
+        // Oculta el elemento
+        scrollToHome.style.display = 'none';
+    }
+});
